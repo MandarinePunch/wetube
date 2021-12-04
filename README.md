0808d78c7015413041feaf9a66bf941844bf794f
Rakku <dudfkr7845@gmail.com>
2021-12-04 23:05:03 +0900

    router-controller

---
+++ README.md
@@ -0,0 +1,13 @@
+/ -> home
+/join ->Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete ->Delete user
+
+/videos/watch ->Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete a comment of a video
--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-app.get("/", (req, res) => {
-  return res.send("I still love you.");
-});
-app.get("/login", (req, res) => {
-  return res.send({ message: "Login here." });
-});
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-const handleListening = () =>
+const handleListening = () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
+};
 
 app.listen(PORT, handleListening); //(포트번호, 서버 시작시 작동 함수)
