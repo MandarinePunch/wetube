174fb783bcf79460ecbd03794ff6a62f2a7bf6e2
Rakku <dudfkr7845@gmail.com>
2022-02-11 00:23:24 +0900

    s3 uploads

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -145,7 +145,7 @@ const handleEnded = (event) => {
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
-video.addEventListener("loadeddata", handleLoadedMetadata);
+video.addEventListener("canplay", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("click", handleVideoClick);
 video.addEventListener("ended", handleEnded);
